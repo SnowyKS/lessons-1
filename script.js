6fe1287d7667f8a911321face7100c5c87a34162
SnowyKS <silovskijegor237@gmail.com>
2026-02-20 22:47:55 +0300

    Добавил в ветку dev файлы

---
+++ script.js
@@ -0,0 +1,10 @@
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+alert("Добро пожаловать в мой проект!");
+
+console.log("Сообщение в консоли. Все работает отлично!");
