4c01bb65ead5fd41694f784257678c4e1628663f
SnowyKS <silovskijegor237@gmail.com>
2026-02-28 23:27:16 +0300

    Добавил второй урок

---
+++ main.js
@@ -0,0 +1,17 @@
+let title = "project"
+let screens = "Простые, Сложные, Интерактивные"
+let screenPrice = 1000
+let rollback = 90
+let fullPrice = 25000
+let adaptive = true
+
+console.log(typeof title)
+console.log(typeof fullPrice)
+console.log(typeof adaptive)
+
+console.log(screens.length)
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`)
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`)
+
+console.log(screens.toLocaleLowerCase().split())
+console.log(fullPrice * (rollback/100))
